2d3ec5e0f01a8ec7ad178ddc717798661553e413
Daren Sipes <daren@teamwilletts.com>
2019-02-19 11:51:19 -0500

    Setting up routes and bootstrap

---
+++ config/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+use Cake\Event\EventManager;
+use Cake\Event\Event;
+use Cake\Http\ServerRequest;
+
+EventManager::instance()->on('Controller.initialize', function (Event $event) {
+    $controller = $event->getSubject();
+    if ($controller->components()->has('RequestHandler')) {
+        $controller->RequestHandler->setConfig('viewClassMap.xlsx', 'CsvView.Spreadsheet');
+    }
+});
+
+ServerRequest::addDetector(
+    'xlsx',
+    [
+        'accept' => ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
+        'param' => '_ext',
+        'value' => 'xlsx',
+    ]
+);
